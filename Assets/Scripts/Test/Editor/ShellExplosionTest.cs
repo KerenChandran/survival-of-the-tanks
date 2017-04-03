a210b449c059f37a07c13e10ab80fe1e0c775adb
Ursula Huang <ursula1207@hotmail.com>
2017-04-03 19:25:08 -0400

    Testing code

---
+++ Assets/Scripts/Test/Editor/ShellExplosionTest.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using System;
+
+public class ShellExplosionTest {
+
+	public GameObject player;
+
+	[Test]
+	public void ManagerTestSimplePasses() {
+		// Use the Assert class to test conditions.
+		Assert.That(2+2==4);
+	}
+
+	[Test]
+	public void TestMinShooting() {
+		string playerName = "player1";
+		GameObject player = GameObject.CreatePrimitive (PrimitiveType.Cube);
+
+		Vector3 position = new Vector3 (-3, 0, 30);
+		Quaternion rotation = Quaternion.Euler (0f, 180f, 0f);
+
+		GameObject shell = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		shell.AddComponent<Rigidbody> ();
+
+		Assert.That (shell != null);
+
+		GameObject p = UnityEngine.Object.Instantiate(player);
+//		GameObject p = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		p.name = playerName;
+		p.transform.position = position;
+		p.transform.rotation = rotation;
+
+		Assert.That (p != null);
+
+		p.AddComponent<TankMovement> ();
+		p.AddComponent<TankShooting> ();
+		p.AddComponent<TankHealth> ();
+
+		TankShooting shooting = p.GetComponent<TankShooting> ();
+		shooting.playerName = playerName;
+		shooting.m_FireTransform = p.transform;
+		shooting.m_Shell = shell.GetComponent<Rigidbody>();
+
+		TankShooting s = p.GetComponent<TankShooting> ();
+		s.setCurrentLaunchForce (TankShooting.m_MinLaunchForce);
+		// Gets an NullReferenceException when trying to test for number of shells created
+//		s.CmdFire ();
+//		object[] explosions = UnityEngine.Object.FindObjectsOfType (typeof(ShellExplosion));
+//		Assert.That (explosions.Length == 1);
+	}
+
+	// A UnityTest behaves like a coroutine in PlayMode
+	// and allows you to yield null to skip a frame in EditMode
+	[UnityTest]
+	public IEnumerator ManagerTestWithEnumeratorPasses() {
+		// Use the Assert class to test conditions.
+		// yield to skip a frame
+		yield return null;
+	}
+}
